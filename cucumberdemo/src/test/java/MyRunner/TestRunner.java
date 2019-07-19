1478477779d1d6ec98e17394abafa28b6e962bab
oussama <oussama.ben.hedia@gmail.com>
2019-07-19 12:06:31 +0100

    aadd

--- cucumberdemo/src/test/java/MyRunner/TestRunner.java
+++ cucumberdemo/src/test/java/MyRunner/TestRunner.java
@@ -7,7 +7,7 @@ import cucumber.api.junit.Cucumber;
 
 	@RunWith(Cucumber.class)
 	@CucumberOptions(
-		features = "C:\\Users\\ASUS\\eclipse-workspace\\cucumberdemo\\src\\main\\java\\Features\\login.feature", //the path of the feature files
+		features = "C:\\Users\\ASUS\\git\\CucumberDemo\\cucumberdemo\\features\\login.feature", //the path of the feature files
 		glue={"stepDefinitions"}, //the path of the step definition files*
 		format= {"pretty","html:test-outout", "json:json_output/cucumber.json", "junit:junit_xml/cucumber.xml"} //to generate different types of reporting
 
